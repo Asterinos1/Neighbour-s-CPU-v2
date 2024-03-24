9644687b15c36451a700d2d74f5869a4920c8c49
Polychronis Stamoulis <59158728+poler2860@users.noreply.github.com>
2024-03-24 18:15:02 +0200

    Update top_entity_module.vhd

--- part0/top_entity_module.vhd
+++ part0/top_entity_module.vhd
@@ -30,12 +30,57 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity top_entity_module is
+    Port ( CLK : in  STD_LOGIC;
+           RST : in  STD_LOGIC;
+           AddrWrite : in  STD_LOGIC_VECTOR (4 downto 0);
+           AddrRead : in  STD_LOGIC_VECTOR (4 downto 0);
+           WriteF : in  STD_LOGIC;
+           ReadF : in  STD_LOGIC;
+           NumberIN : in  STD_LOGIC_VECTOR (15 downto 0);
+           NumberOUT : out  STD_LOGIC_VECTOR (15 downto 0);
+           Valid : out  STD_LOGIC);
 end top_entity_module;
 
 architecture Behavioral of top_entity_module is
 
+	component memory_controller is
+		Port( CLK,RST,Wf,Rf: in std_logic;
+				AddrW, AddrR: in std_logic_vector(4 downto 0);
+				WEf: out std_logic_vector(0 downto 0);
+				AddrOUT: out std_logic_vector(4 downto 0)
+				);
+	end component;
+	
+	component memory is 
+		Port ( rsta, clka : in std_logic;
+					wea: in std_logic_vector(0 downto 0); -- In MEM vhd this flag is set as vector of 1 bit for smrsn idk
+					addra: in std_logic_vector(4 downto 0);
+					dina: in std_logic_vector(15 downto 0);
+					douta : out std_logic_vector(15 downto 0)
+				);
+	end component;
+	
+	signal we_s : std_logic_vector(0 downto 0);
+	signal addr_s : std_logic_vector (4 downto 0);
+	
 begin
 
+	u1: memory_controller Port Map(CLK => CLK,
+						RST => RST,
+						AddrW => AddrWrite,
+						AddrR => AddrRead,
+						Wf => WriteF,
+						Rf => ReadF);
+						
+	u2: memory Port Map (clka => CLK,
+						rsta => RST,
+						douta => NumberOUT,
+						dina => NumberIN,
+						wea => we_s,
+						addra => addr_s);
+						
+	Valid <= ReadF;
+
+	
 
 end Behavioral;
-
