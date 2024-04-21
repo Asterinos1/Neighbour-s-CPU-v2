4f91ff79929459b36f746844d269a0247454c15a
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-04-21 21:50:07 +0300

    Update reg_mux.vhd

--- part1/RF/reg_mux.vhd
+++ part1/RF/reg_mux.vhd
@@ -2,17 +2,17 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.matrices.all;
 
-entity reg_mux is
-	port(
-		reg_in: in MATRIX(0 to 31);
-		sel: in integer range 0 to 31;
-		reg_out: out std_logic_vector(31 downto 0)
-		);
-end reg_mux;
+entity generic_mux is
 
-architecture Behavioral of reg_mux is
+	Port ( input : in  MATRIX (0 to 31);
+           sel : in  integer range 0 to 31;
+           output : out  STD_LOGIC_VECTOR (m-1 downto 0));
+end generic_mux;
+
+architecture Behavioral of generic_mux is
 
 begin
-	reg_out <= reg_in(sel);
-end Behavioral;
 
+	output <= input(sel);
+
+end Behavioral;
