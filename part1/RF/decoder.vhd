ea5adeb3b5d012a073ac1642a8af5b00a9f520e1
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-04-21 22:39:17 +0300

    Update decoder.vhd

--- part1/RF/decoder.vhd
+++ part1/RF/decoder.vhd
@@ -16,10 +16,7 @@ begin
 	
 		X <= X"00000000";
 		X(conv_integer(A)) <= '1';
-		-- The above statements are "squeezed" into the following line so that the after statement can be applied correctly.
-		-- However this may cause problems with synthesizability, due to non static index assignment. If this happens, comment
-		-- the following line out and uncomment the 2 lines above.
-		--X <= (conv_integer(A) => '1', others => '0');	-- 1 at input position, 0 everywhere else.
+		
 	end process;
 
 end Behavioral;
