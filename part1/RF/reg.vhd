7a8ad1876b0660d4c36e3f45e2c3d0e2d3128730
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-04-21 22:06:46 +0300

    Update reg.vhd

--- part1/RF/reg.vhd
+++ part1/RF/reg.vhd
@@ -1,17 +1,3 @@
-----------------------------------------------------------------------------------
--- Module Name:    register - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -23,26 +9,20 @@ entity reg is
            Dataout : out  STD_LOGIC_VECTOR (31 downto 0));
 end reg;
 
-	
 architecture Behavioral of reg is
-
-		signal temp : STD_LOGIC_VECTOR (31 downto 0);
+	signal temp : STD_LOGIC_VECTOR (31 downto 0);
 
 begin
-
--- Design taken from lab material "flip-flop with Q" and adapted 
--- Synchronous RST Register with Enable signal
-
 	process (CLK,WE,RST,Datain)
 	begin
 		if rising_edge(CLK) then
-			if 	RST='1' 	then	temp <= (others => '0');	-- reset is "active high"
-			elsif WE='1' 	then	temp <= Datain;				-- elsif because WE and RST should not be on at the same time
+			if 	RST='1' then	temp <= (others => '0');
+			elsif WE='1' 	then	temp <= Datain;			
 			else temp <= temp;
 			end if;
 		end if;    
 	end process;
 
-		Dataout <= temp;
+	Dataout <= temp;
 end Behavioral;
 
