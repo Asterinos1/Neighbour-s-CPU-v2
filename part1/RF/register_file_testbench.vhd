7022260a783a3bee84cf0d729829decb9b388d1a
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-04-21 22:08:04 +0300

    Update register_file_testbench.vhd

--- part1/RF/register_file_testbench.vhd
+++ part1/RF/register_file_testbench.vhd
@@ -1,24 +1,3 @@
---------------------------------------------------------------------------------
--- Project Name:  lab1_organwsh_RF
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: register_file
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
