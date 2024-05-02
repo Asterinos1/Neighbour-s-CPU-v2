c80680c0f51ac861e9ff4e7702287ffcd2daa91f
Asterinos Karalis <79656046+Asterinos1@users.noreply.github.com>
2024-05-02 19:42:23 +0300

    Update Instr_ext.vhd

--- part1/Instr_ext.vhd
+++ part1/Instr_ext.vhd
@@ -19,33 +19,36 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL; -- Properly included for arithmetic operations
 
 entity Instr_ext is
-	Port ( Opcode : in STD_LOGIC_VECTOR (1 downto 0);
-			 Instr : in  STD_LOGIC_VECTOR (15 downto 0);
-          Immed : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( Opcode : in STD_LOGIC_VECTOR (1 downto 0);
+           Instr : in  STD_LOGIC_VECTOR (15 downto 0);
+           Immed : out  STD_LOGIC_VECTOR (31 downto 0));
 end Instr_ext;
 
 architecture Behavioral of Instr_ext is
-
+	signal temp: STD_LOGIC_VECTOR(31 downto 0);
 begin
-
-	process(Opcode, Instr)
-	begin
-		case Opcode is
-			when "00" =>
-				Immed <= resize(Instr, Immed'length);				
-		end case;
-	end process;
-
+    process(Opcode, Instr)
+    begin
+        case Opcode is
+            when "00" => -- Simple zerofill, no shifting
+					temp <= x"0000" & Instr;
+				when "01" => -- zerofill with shift left
+					temp <=  Instr & x"0000";
+				when "10" => --se without shift
+               temp(15 downto 0) <= Instr;
+					temp(31 downto 16) <= (others => Instr(15));
+				when "11" => --se with shift
+					temp(15 downto 0) <= Instr;
+					temp(31 downto 16)<= (others => Instr(15));
+				when others => --default stays dones nothing
+					null;
+        end case;
+    end process;
+	
+	--temporary fix
+	Immed <= (temp(29 downto 0) & "00") when opcode="11" else temp;
 end Behavioral;
 
